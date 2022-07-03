5d101299181b981931558dc935a89200cf5e4381
jashwanth <jashwanth328910@gmail.com>
2022-07-03 12:20:31 +0530

    update secondHeightest

--- assignments/secondHeighest.js
+++ assignments/secondHeighest.js
@@ -1,16 +1,17 @@
 const secondHighestValue = function (arr) {
   let firstHighest = -1e10;
   let secondHighest = -1e10;
+  // arr = new Set(arr);
   for (i of arr) {
     if (i > firstHighest) {
       secondHighest = firstHighest;
       firstHighest = i;
-    } else {
+    } else if (i !== firstHighest) {
       secondHighest = Math.max(secondHighest, i);
     }
   }
   return secondHighest;
 };
 
-const a = [1, 2, 13, 4, 5, 6, 7, 8, 9, 10];
+const a = [1, 2, 10, 4, 5, 10, 7, 10, 9, 1, 10];
 console.log(secondHighestValue(a));
