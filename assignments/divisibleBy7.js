8fea109a07d7cd6c3841436b3fb3a6c697032cde
jashwanth <jashwanth328910@gmail.com>
2022-07-03 12:46:02 +0530

    adding divisble_by_7

---
+++ assignments/divisibleBy7.js
@@ -0,0 +1,16 @@
+// Steps
+// we need to find elements divisble by n
+// iterate over the arr and take out the elements that are divisble and retrun them
+// the best to do the above operation is using filter. It filters the elements out of the array based on given condition
+
+// T.C = O(n) one iteration
+// S.C = O(k) here k is the no.of elements that are divisible by n
+
+// code
+const divisbleBy_7 = function (arr, n) {
+  return arr.filter((ele) => ele % n === 0);
+};
+
+const a = [23, 34, 56, 43, 16, 24, 81, 18];
+const n = 9;
+console.log(divisbleBy_7(a, n));
